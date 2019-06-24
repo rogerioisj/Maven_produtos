0ff3b849cda9c945d4350a6cb3def22fed0274f0
roger <roger@roger-notebook>
2019-06-23 23:43:55 -0300

    Projeto encerrado

---
+++ produtos/src/main/java/br/com/alura/maven/App.java
@@ -0,0 +1,12 @@
+package br.com.alura.maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	public static void main(String[] args) {
+		Produto bala = new Produto("Bala oldschool", 0.01);
+		System.out.println(bala);
+	}
+}
---
+++ produtos/src/main/java/br/com/alura/maven/Produto.java
@@ -0,0 +1,31 @@
+package br.com.alura.maven;
+
+public class Produto {
+
+	private final String nome;
+	private final double preco;
+
+	public Produto(String nome, double preco) {
+		super();
+		this.nome = nome;
+		this.preco = preco;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public double getPreco() {
+		return preco;
+	}
+
+	@Override
+	public String toString() {
+		return "Produto: " + this.nome + "\nPreço: " + this.preco;
+	}
+	
+	public double getPrecoComImposto() {
+        return preco * 1.1;
+    }
+
+}
